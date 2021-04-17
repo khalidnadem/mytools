1a7fc35be75edc83014d9b6c64bab7d249fbd9b8
khalidnadem <khalidnadem@gmail.com>
2021-04-17 19:19:45 +0100

    update

--- R/clean_col_name.R
+++ R/clean_col_name.R
@@ -1,3 +1,13 @@
+#' Clean Column Names
+#'
+#' @param df a data frame
+#'
+#' @return A data frame
+#' @export
+#'
+#' @examples
+#' df <- data.frame(tmPP = c(1:5), YnUU = c(1:5))
+#' df <- clean_col_name(df)
 clean_col_name <- function(df) {
   df <- janitor::clean_names(df)
 }
